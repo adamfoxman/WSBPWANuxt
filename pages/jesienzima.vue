beef5556155f7641dcfdf293d79121fd0f940f6a
Piotr 74167 <pkowalecki@gmail.com>
2024-04-12 11:21:26 -0100

    Dodano: - wspólny styl dla stron z kategoriami - po 3 elementy na stronę

--- pages/jesienzima.vue
+++ pages/jesienzima.vue
@@ -1,3 +1,51 @@
 <template>
-<h3>Jesień zima</h3>
+  <h3>Jesień zima</h3>
+  <div class="product">
+    <section>
+        <div class="products-container">
+          <div v-for="product in products" :key="product.id" class="p-4">
+            <img class="product product-image" :src="product.image" />
+            <div class="mt-4">
+              <h3 class="product-name">
+                {{ product.name }}
+              </h3>
+              <p class="mt-1">
+                {{ product.price }} zł
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Bluzka",
+          price: "469.99",
+          image: "/img/jesienzima_0.jpg",
+        },
+        {
+          id: 2,
+          name: "Sweter",
+          price: "399.99",
+          image: "/img/jesienzima_1.jpg",
+        },
+        {
+          id: 3,
+          name: "Sweter",
+          price: "359.99",
+          image: "/img/jesienzima_2.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
--- pages/stylizacje-codzienne.vue
+++ pages/stylizacje-codzienne.vue
@@ -1,3 +1,50 @@
 <template>
-<h3>Stylizacje codzienne</h3>
-</template>
+  <h3>Stylizacje codzienne</h3>
+  <div class="product">
+    <section>
+      <div class="products-container">
+        <div v-for="product in products" :key="product.id" class="p-4">
+          <img class="product product-image" :src="product.image" />
+          <div class="mt-4">
+            <h3 class="product-name">
+              {{ product.name }}
+            </h3>
+            <p class="mt-1">
+              {{ product.price }} zł
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Wzorek kwiatowy",
+          price: "269.90",
+          image: "/img/codzienna_2.jpg",
+        },
+        {
+          id: 2,
+          name: "Wzorek sportowy 2",
+          price: "399.90",
+          image: "/img/codzienna_1.jpg",
+        },
+        {
+          id: 3,
+          name: "Bluzka",
+          price: "69.99",
+          image: "/img/codzienna_0.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
