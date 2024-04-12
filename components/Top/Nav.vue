0b4126f9181e146e5e88b3f12fd3634790b3fb54
Piotr 74167 <pkowalecki@gmail.com>
2024-04-12 09:08:26 -0100

    Dodano: - podstawowe style do nagłówków

--- components/Top/Nav.vue
+++ components/Top/Nav.vue
@@ -17,23 +17,66 @@ const showMobileMenu = () => {
       </a>
     </div>
     <div id="navbarBasicExample" :class="{'navbar-menu': !mobileMenu}">
-    <div class="navbar-start">
-        <NuxtLink class="navbar-item" to="/jesienzima">
-          KOLEKCJA ZIMOWA
-        </NuxtLink>
-        <NuxtLink class="navbar-item" to="/stylizacje-wieczorowe">
-          STYLIZACJE WIECZOROWE
-        </NuxtLink>
-        <NuxtLink class="navbar-item" to="/stylizacje-codzienne">
-          STYLIZACJE CODZIENNE
-        </NuxtLink>
-        <NuxtLink class="navbar-item" to="/kolekcja-swiateczna">
-          KOLEKCJA ŚWIĄTECZNA
-        </NuxtLink>
-        <NuxtLink class="navbar-item" to="/wyprzedaz">
-          WYPRZEDAŻ
-        </NuxtLink>
+      <div class="navbar-start">
+        <ul>
+          <li>
+            <NuxtLink class="navbar-item" to="/jesienzima">
+              KOLEKCJA ZIMOWA
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class="navbar-item" to="/stylizacje-wieczorowe">
+              STYLIZACJE WIECZOROWE
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class="navbar-item" to="/stylizacje-codzienne">
+              STYLIZACJE CODZIENNE
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class="navbar-item" to="/kolekcja-swiateczna">
+              KOLEKCJA ŚWIĄTECZNA
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class="navbar-item" to="/wyprzedaz">
+              WYPRZEDAŻ
+            </NuxtLink>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<style scoped lang="scss">
+.navbar-menu {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1140px;
+  padding-left:40px;
+  padding-right:40px;
+  padding-bottom:40px;
+  background:#fff;
+}
+
+.navbar-item {
+  font-size: 1.6rem;
+  line-height: 1.2;
+  font-weight: 300;
+  color: #000;
+  padding: 12px 0 25px;
+  font-family: 'Merriweather',serif;
+  margin: 0 15px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 10px;
+}
+
+</style>
